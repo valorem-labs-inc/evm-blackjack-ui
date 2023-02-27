ed1101b8368f09e181aa305beb9e091ea0e0dcb8
nickadamson <naburner@protonmail.com>
2023-02-27 12:16:03 -0700

    chore: set up linters

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,13 +1,51 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
   settings: {
     react: {
       version: "detect",
     },
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
+    tsconfigRootDir: __dirname,
+    project: ["node_modules/tsconfig/base.json"],
   },
+  plugins: [],
+  extends: [
+    "turbo",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+    "plugin:@next/next/core-web-vitals",
+    "prettier",
+  ],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    "import/no-extraneous-dependencies": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/function-component-definition": "off",
+    "react/destructuring-assignment": "off",
+    "react/jsx-filename-extension": "off",
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true },
+    ],
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+  },
+  ignorePatterns: [".eslintrc.js", "next.config.js", "index.js"],
 };
