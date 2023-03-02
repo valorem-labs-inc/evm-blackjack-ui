2a35fc6f5733ce5d40ebeb9b2dcc44c7baaaa243
nickadamson <naburner@protonmail.com>
2023-03-01 21:33:09 -0700

    fix: poap modulo

--- apps/frontend/src/pages/api/poap/[id].tsx
+++ apps/frontend/src/pages/api/poap/[id].tsx
@@ -5,5 +5,5 @@ const links: string[] = JSON.parse(process.env.POAPLINKS!) as string[];
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
   console.log("redirecting tester to POAP Link");
-  res.status(200).redirect(links[Number(id)]);
+  res.status(200).redirect(links[Number(id) % 149]);
 }
